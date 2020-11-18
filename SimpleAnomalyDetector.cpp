944040218f2a27fa343d39a0b67a9349a701f739
Tal Klein <talkleinw@gmail.com>
2020-11-18 10:39:02 +0200

    added comments to all files

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -26,7 +26,7 @@ bool features_already_in(vector<correlatedFeatures> cf, string fi, string fj) {
 }
 
 /**
- * @brief Function learns which column in the time series are correlative and saves its findings in the member cf
+ * @brief Function learns which columns in the time series are correlative and saves its findings in the member cf
  * 
  * @param ts 
  */
@@ -43,9 +43,9 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
 				float* fi = ts.get_col_by_index(i);
 				float* fj = ts.get_col_by_index(j);
 				info.corrlation = std::abs(pearson(fi,fj,feature_size));  //calculate correlation between 2 features fi and fj
-				if (info.corrlation >= 0.9) {   //verifying if the abs correlation of the two features is in fact correlative (over 0.9)
+				if (info.corrlation >= 0.9) {   //verifying if the abs correlation of the two features is in fact correlative (over or equal to 0.9)
 					const string featurei = (ts.get_features())[i];    //save first feature by name
-					const string featurej = (ts.get_features())[j];    // save second feature by name  
+					const string featurej = (ts.get_features())[j];   // save second feature by name  
 					if (features_already_in(SimpleAnomalyDetector::cf,featurei,featurej)) {
 						continue;
 					}
@@ -64,24 +64,24 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
 		string featr2_str = SimpleAnomalyDetector::cf[i].feature2;
 		int feat_1_index = ts.get_index_of_feature(featr1_str);    //find index of feature 1 in the time series 
 		int feat_2_index = ts.get_index_of_feature(featr2_str);    //find index of feature 2 in the time series 
-		float* f1 = ts.get_col_by_index(feat_1_index);    //get the feature1 column-data by index
-		float* f2 = ts.get_col_by_index(feat_2_index);    //get the feature1 column-data by index
+		float* f1 = ts.get_col_by_index(feat_1_index);    //get the feature1 (column-data) by index
+		float* f2 = ts.get_col_by_index(feat_2_index);    //get the feature1 (column-data) by index
 		for (int j=0; j<feature_size; j++) {
 			float x = f1[j];
 			float y = f2[j];
-			p_array[j] = new Point(x,y);    //add point to array --> all the (x,y): such that x is from feature1 &&  y is from feature2
+			p_array[j] = new Point(x,y);    //add point to array --> all the (x,y): such that x is from feature1 && y is from feature2
 		}
 		vec_of_point_arrays.push_back(p_array);    //save array of all points for feature1 and feature2 in a vector.
 	}
 	for (int i=0; i<NUM_OF_CORRELATED_FEATURES; i++) {
 		SimpleAnomalyDetector::cf[i].lin_reg = linear_reg(vec_of_point_arrays[i], feature_size);    //save the linear regression for each couple of correlated features.
 	}
-	//for all arrays of points and their linear reg line: calculate the max deviation
+	//for all arrays of points and their linear regression line, calculate the max deviation:
 	for (int i = 0; i < NUM_OF_CORRELATED_FEATURES; i++) {
 		float max_deviation = 0.0;    //intialize max deviation for each correlated features.
 		for (int j = 0; j<feature_size; j++) {
 			float deviation;
-			deviation = dev(*(vec_of_point_arrays[i][j]), SimpleAnomalyDetector::cf[i].lin_reg);    //find deviation between a given point and the linear regression of 2 correlative features.
+			deviation = dev(*(vec_of_point_arrays[i][j]), SimpleAnomalyDetector::cf[i].lin_reg);    //find deviation between a point from the point array and the linear regression of 2 correlative features.
 			if (max_deviation < deviation) {
 				max_deviation = deviation;
 			}
@@ -102,20 +102,21 @@ vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts) {
 	vector<AnomalyReport> vec_report;
 	const int size = ts.get_num_of_rows();
 	const int row_size = ts.get_num_of_features();
-	for (int i =0; i< cf.size(); i++) {
+	for (int i =0; i< cf.size(); i++) {    //iterate over all the elements in the cf member
 		if (SimpleAnomalyDetector::cf[i].corrlation >= 0.9) {
 			for (int j =1; j<= size; j++) {
 				 float* row = new float[row_size];
 				 ts.fill_row(j , row);    //get row j
 				 const int feature1_index = ts.get_index_of_feature(SimpleAnomalyDetector::cf[i].feature1);    //find the index in which feature1 is placed in the row
-			     const int feature2_index = ts.get_index_of_feature(SimpleAnomalyDetector::cf[i].feature2);    //find the index in which feature1 is placed in the row   
+			     const int feature2_index = ts.get_index_of_feature(SimpleAnomalyDetector::cf[i].feature2);    //find the index in which feature2 is placed in the row   
 				 float x = row[feature1_index];
 				 float y = row[feature2_index];
-				 Point* p = new Point(x,y);    //create a point that the x value is from feature 1 and y value from feature2
-				 float deviation  = dev(*p,SimpleAnomalyDetector::cf[i].lin_reg);    //calculate deviation from the linear regression line that was calculated in the learnNormal function    
+				 Point* point = new Point(x,y);    //create a point that the x value is from feature 1 and y value from feature2
+				 float deviation  = dev(*point,SimpleAnomalyDetector::cf[i].lin_reg);    //calculate deviation from the linear regression line that was calculated in the learnNormal function 
+				 delete point;    //free allocated Point.
 				 if (deviation > SimpleAnomalyDetector::cf[i].threshold) {    //verifying if the deviation is bigger than the threshold found in the learnNormal function
-					 const string description = SimpleAnomalyDetector::cf[i].feature1 + "-" + SimpleAnomalyDetector::cf[i].feature2;
-					 const long timestep = j;
+					 const string description = SimpleAnomalyDetector::cf[i].feature1 + "-" + SimpleAnomalyDetector::cf[i].feature2;   
+					 const long timestep = j;    //update the time step to be the index of the line in the file that is read at the moment
 					 AnomalyReport report(description,timestep);    //create report about the anomaly found
 					 vec_report.push_back(report);    //add report to the reports vector.
 				 }
